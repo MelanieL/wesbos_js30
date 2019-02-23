081d7e6d8cec7aa85e04aceefe9d6f1e7918e591
MelanieL <melanieleeking@gmail.com>
2019-02-23 14:38:20 -0500

    Completed ex05 Flex Panel Gallery.

--- 05 - Flex Panel Gallery/main.js
+++ 05 - Flex Panel Gallery/main.js
@@ -1,3 +1,21 @@
 $( document ).ready (function() {
+    const panels = document.querySelectorAll('.panel');
+
+    function toggleOpen() {
+        this.classList.toggle('open');
+    }
+
+    function toggleActive(e) {
+        // This console log will tell us which transitions to look at
+        console.log(e.propertyName);
+        // Important to say "if includes flex because for Safari it's different than other browsers (flex-grow vs just flex), so this will catch all of them"
+        if(e.propertyName.includes('flex')) {
+            this.classList.toggle('open-active');
+        }
+    }
+
+    // Loop over each panel, listen for a click, find and run function
+    panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+    panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 
 });
