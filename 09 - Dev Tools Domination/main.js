a8acdb0ea214a0e9a0b35474d379b905de825078
MelanieL <melanieleeking@gmail.com>
2019-03-12 14:07:04 -0400

    Finished ex 09 - Dev Tools.

--- 09 - Dev Tools Domination/main.js
+++ 09 - Dev Tools Domination/main.js
@@ -26,14 +26,40 @@ console.error('Shit!');
 console.info('Crocodiles eat 3-4 people per year');
 
 // Testing
+const p = document.querySelector('p');
+console.assert(p.classList.contains('ouch'), 'That is wrong!');
+
+console.assert(1 === 1, 'That is wrong!');
 
 // clearing
+console.clear();
 
 // Viewing DOM Elements
+// To take a look at all things that live on that element (class list, etc.)
+console.dir(p);
 
 // Grouping together
+dogs.forEach(dog => {
+    console.group(`${dog.name}`);
+    // can also use console.groupCollapsed so that the info is less cluttered and tabs are collapsed
+    console.log(`This is ${dog.name}`);
+    console.log(`${dog.name} is ${dog.age} years old`);
+    console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+    console.groupEnd(`${dog.name}`);
+});
 
 // counting
+// Will count how many time to use
+console.count('Wes');
 
 // timing
+// To See how long something takes
+// There is also performance.now in browser
+console.time('fetching data');
+fetch('https://api.github.com/users/wesbos')
+    .then(data => data.json())
+    .then(data => {
+        console.timeEnd('fetching data');
+        console.log(data);
+    });
 
