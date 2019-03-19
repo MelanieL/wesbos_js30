6d7eca31de2a2b4f298560c235532cbb879bc836
MelanieL <melanieleeking@gmail.com>
2019-03-19 14:15:04 -0400

    Completed ex 13 - Slide in on Scroll.

--- 13 - Slide in on Scroll/main.js
+++ 13 - Slide in on Scroll/main.js
@@ -1,3 +1,4 @@
+// Without debounce, when you scroll the function gets called 50+ times which is too much. With debounce, it will only run so many times in a set amount of time. There are frameworks for this problem, but this tutorial is vanilla js so this is how we are dealing with it.
 function debounce(func, wait = 20, immediate = true) {
     var timeout;
     return function () {
@@ -12,3 +13,31 @@ function debounce(func, wait = 20, immediate = true) {
         if (callNow) func.apply(context, args);
     };
 }
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+    // console.log(e);
+    // console.count(e);
+    // To get a reading of how far down the page you are scrolled:
+    // console.log(window.scrollY + window.innerHeight);
+    sliderImages.forEach(sliderImage => {
+        // Haldway through the image
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        // console.log(slideInAt);
+        // Bottom of the image:
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
+    });
+
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
+
+// Rule: When page is scrolled to show 50% of area where image should be, that's when we want our image to appear
