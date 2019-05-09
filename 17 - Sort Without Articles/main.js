68977672cf21476260032d71b2ab0f1027333837
MelanieL <melanieleeking@gmail.com>
2019-05-09 19:33:29 -0400

    Add completed files for ex 16 - sort without articles

--- 17 - Sort Without Articles/main.js
+++ 17 - Sort Without Articles/main.js
@@ -1,1 +1,21 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function strip(bandName) {
+    // This regex expression says when it starts with (that's what the carrot is for) a or the or an, the i makes it insensitive, meaning it doesn't care about caps
+    return bandName.replace(/^(a |the |an)/i, '').trim();
+}
+
+const sortedBands = bands.sort(function(a, b) {
+    if (strip(a) > strip(b)) {
+        return 1;
+    } else {
+        return -1;
+    }
+});
+
+document.querySelector('#bands').innerHTML = 
+    sortedBands
+        .map(band => `<li>${band}</li>`)
+        .join('');
+
+console.log(sortedBands);
